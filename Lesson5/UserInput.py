ea5fd45ba184e50d4560bbb425c1b594dda3503d
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2024-10-28 13:30:06 +0200

    python for beginners

---
+++ Lesson5/UserInput.py
@@ -0,0 +1,43 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+print("")
+playerchoice = input("Enter...\n1 for Rock,\n2 for Paper,\n3 for Scissors: ")
+
+player = int(playerchoice)
+
+if player < 1 or player > 3:
+    sys.exit("You must enter 1,2 or 3.")
+
+computerchoice = random.choice("123")
+
+computer = int(computerchoice)
+
+print("")
+# print("You chose " + playerchoice + ".")
+# print("Computer chose " + computerchoice +".")
+# print("You chose " + str(RPS(player)) + ".")
+# print("Computer chose " + str(RPS(computer)) +".")
+print("You chose " + str(RPS(player)).replace("RPS.","") + ".")
+print("Computer chose " + str(RPS(computer)).replace("RPS.","") +".")
+print("")
+
+if player == 1 and computer == 3:
+    print("😜 You win!")
+elif player == 2 and computer == 1:
+    print("😜 You win!")
+elif player == 3 and computer == 2:
+    print("😜 You win!")
+elif player == computer:
+    print("🙌 Tie game!")
+else: 
+    print("🤷‍♂️ Computer wins!")
+
+
