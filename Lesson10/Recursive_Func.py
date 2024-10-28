e35cc9950730488053c876eff7e50ac599c39bd9
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2024-10-28 20:54:23 +0200

    Recursive Functions

---
+++ Lesson10/Recursive_Func.py
@@ -0,0 +1,13 @@
+# Recursion happens when a function calls itself
+def add_one(num):
+
+    if (num >= 9):
+        return num + 1
+    
+    total = num + 1
+    print(total)
+
+    add_one(total)
+
+mynewtotal = add_one(0)
+print(mynewtotal)
